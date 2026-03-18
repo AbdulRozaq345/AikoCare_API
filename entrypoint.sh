a99a049628590a2d5aac2df54d3137cf4b308cf0
rozaq <auliavera203@gmail.com>
2026-03-19 03:09:37 +0700

    oho

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# Jalankan PHP-FPM di background
-echo "Starting PHP-FPM..."
 php-fpm -y /assets/php-fpm.conf -D
-
-# Jalankan Nginx di foreground (utama)
-echo "Starting Nginx..."
 nginx -g "daemon off;" -c /app/nginx.conf
